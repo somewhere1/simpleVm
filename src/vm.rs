7a2b1b5be16840e9b73bc72f881cca1667336d4d
somewhere1 <1786459296@qq.com>
2025-04-19 10:28:29 -0500

    update

--- src/vm.rs
+++ src/vm.rs
@@ -1,4 +1,6 @@
 use crate::memory::{LinearMemory,Addressable};
+use std::collections::HashMap;
+
 //寄存器元组
 #[derive(Debug)]
 #[repr(u8)]
@@ -31,6 +33,7 @@ pub enum Op{
     PopRegister(Register),
     AddStack,
     AddRegister(Register,Register),
+    Signal(u8),
 
 }
 impl Op{
@@ -41,6 +44,11 @@ impl Op{
     //     x==other.value
     // }
 }
+
+fn parse_instruction_arg(ins:u16) -> u8{
+    ((ins&0xff00) >> 8) as u8
+}
+
 
 fn parse_instruction(ins: u16) -> Result<Op,String>{
 
@@ -58,26 +66,29 @@ fn parse_instruction(ins: u16) -> Result<Op,String>{
             Register::from_u8(reg as u8)
                 .ok_or(format!("unknown register 0x{:X}",reg))
                 .map(|r| Op::PopRegister(r))
-            /*if let Some(r) = Register::from_u8(reg as u8){
-                Ok(Op::PopRegister(r))
-            }
-            else{
-                Err(format!("unknown register 0x{:X}",reg))
-            }*/
                     
         },
         x if  x==Op::AddStack.value() => {
             Ok(Op::AddStack)
+        }
+
+        x if x==Op::Signal(0).value() =>{
+            let arg = parse_instruction_arg(ins);
+            Ok(Op::Signal(arg))
         }
         _ => Err(format!("Unknown op 0x{:X}",op))
     }
 }
 //
 
+
+pub type SignalFunction = fn(&mut Machine) -> Result<(),String>;
 //16bit虚拟机 结构体
 pub struct Machine{
 
     registers:[u16;8],
+    signal_handlers:HashMap<u8,SignalFunction>,
+    pub halt:bool,
     pub memory:Box<dyn Addressable>,
 
 }
@@ -88,6 +99,8 @@ impl Machine{
     pub fn new() -> Self{
         Machine{
             registers:[0;8],
+            halt:false,
+            signal_handlers:HashMap::new(),
             memory: Box::new(LinearMemory::new(8*1024)),
         }
     }
@@ -95,6 +108,11 @@ impl Machine{
     pub fn get_register(&self,r:Register) -> u16{
         self.registers[r as usize]
     }
+
+    pub fn define_handler(&mut self,index:u8,f:SignalFunction){
+        self.signal_handlers.insert(index,f);
+    }
+
     pub fn pop(&mut self,) -> Result<u16,String>{
         //Pop.we have to read the stack pointer first
         let sp = self.registers[Register::SP as usize] -2;
@@ -117,10 +135,12 @@ impl Machine{
         self.registers[Register::SP as usize] += 2;
         Ok(())
     }
+    
+
     pub fn step(&mut self) -> Result<(),String>{
         //枚举索引的隐式转换，会让人疑惑，取出程序计数器
         let pc = self.registers[Register::PC as usize];
-        let instruction = self.memory.read2(pc).unwrap();
+        let instruction = self.memory.read2(pc).ok_or(format!("pc read file @ {:X}",pc))?;
         self.registers[Register::PC as usize ] = pc + 2;
         //Instruction  = [ 0 0 0 0 0 0 0 0 | 0 0 0 0 0 0 0 0 ]
         //                 operation       |ARG(s)
@@ -150,7 +170,13 @@ impl Machine{
             Op::AddRegister(r1,r2) =>{
                 self.registers[r1 as usize] += self.registers[r2 as usize];
                 Ok(())
-            }
+            },
+            Op::Signal(signal) => {
+                let sig_fn = self.signal_handlers
+                    .get(&signal).
+                    ok_or(format!("unkown signal {:X}",signal))?;
+                sig_fn(self)
+            },
         }
         //println!("{} & {}",instruction,pc);
 
